21a04fab82a5a9dbdd62dfc96b561630542b78b1
Richard Ockerby <richardockerby@gmail.com>
2025-05-04 19:56:33 +0100

    Add culture queries to v15 Add content to v15 to test with

--- src/Umbraco.Community.RollbackPreviewer/wwwroot/App_Plugins/UmbracoCommunityRollbackPreviewer/rollback-previewer.js
+++ src/Umbraco.Community.RollbackPreviewer/wwwroot/App_Plugins/UmbracoCommunityRollbackPreviewer/rollback-previewer.js
@@ -5,7 +5,7 @@ const o = [
     name: "Rollback Previewer",
     alias: "Umb.Modal.Rollback",
     type: "modal",
-    element: () => import("./rollback-previewer-modal.element-PifQ2g-c.js")
+    element: () => import("./rollback-previewer-modal.element-J8aaGtSp.js")
   }
 ], a = [
   ...o
