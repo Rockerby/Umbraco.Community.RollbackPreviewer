a52f6555f8813470e1791263c67421daa39ce276
Richard Ockerby <richard@arjo.dev>
2025-12-02 22:53:18 +0000

    Regenerate files (relative path issue)

---
+++ src/Umbraco.Community.RollbackPreviewer/wwwroot/App_Plugins/UmbracoCommunityRollbackPreviewer/rollback-previewer.js
@@ -0,0 +1,16 @@
+import { umbExtensionsRegistry as e } from "@umbraco-cms/backoffice/extension-registry";
+e.unregister("Umb.Modal.Rollback");
+const o = [
+  {
+    name: "Rollback Previewer",
+    alias: "Umb.Modal.Rollback",
+    type: "modal",
+    element: () => import("./rollback-previewer-modal.element-DCHM2ZUs.js")
+  }
+], a = [
+  ...o
+];
+export {
+  a as manifests
+};
+//# sourceMappingURL=rollback-previewer.js.map
